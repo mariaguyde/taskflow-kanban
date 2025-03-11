e87aa3a9a55edf6ec38f18f029412a8dd821a212
Maria <mariaguydefontgalland@gmail.com>
2025-03-11 10:14:54 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # taskflow-kanban
 Gestionnaire de tâches
+
+cd taskflow-kanban
+npm install
+npm run dev
+
+tech stack : react, vite
---
+++ taskflow-kanban/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+import Table from './components/Table'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header/>
+      <Table/>
+    </>
+  )
+}
+
+export default App
