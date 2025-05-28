233bdbadc7e861c359694f4b3ab8c36ccce3acce
Maria <mariaguydefontgalland@gmail.com>
2025-05-28 21:28:30 +0200

    (feat:) gestion des champs vides lors de l'envoi du formulaire

--- taskflow-kanban/src/components/CreateAccount/Createaccount.jsx
+++ taskflow-kanban/src/components/CreateAccount/Createaccount.jsx
@@ -5,12 +5,12 @@ import { Link } from "react-router-dom";
 
 function CreateAccount() {
 
-    const [accountInfos, setAccountInfos] = useState({mail:"", password:""});
+    const [accountInfos, setAccountInfos] = useState({pseudo:"", password:""});
 
     const handleChange = (event) => {
         switch(event.target.id) {
-            case 'email':
-                accountInfos.mail = event.target.value;
+            case 'pseudo':
+                accountInfos.pseudo = event.target.value;
                 setAccountInfos(accountInfos);
                 break;
             case 'password':
@@ -20,6 +20,7 @@ function CreateAccount() {
             case 'submit':
                 console.log(accountInfos);
                 console.log('création dun nouveau user dans mongodb');
+                checkAccountInfos();
                 break;
             default:
                 console.log("problème survenu au niveau des paramètres !");
@@ -28,13 +29,26 @@ function CreateAccount() {
         console.log(accountInfos);
     }
 
+    const checkAccountInfos = () => {
+        if (accountInfos.pseudo.length === 0  || accountInfos.password.length === 0) { // non valide
+            // affichage de l'erreur
+            console.log(document.getElementById('createAccountContainer__errorMsg'));
+            document.getElementById('createAccountContainer__errorMsg').style.display = "block";
+        }
+        else {
+            document.getElementById('createAccountContainer__errorMsg').style.display = "none";
+        }
+    }
+
+
   return (
     <div className={styles.createAccountContainer}>
          <div className={styles.container_formAddColumns}>
             <h4>Créer mon compte</h4>
-            <input type="text" id="email"  className={styles.input} placeholder="Email" onChange={handleChange}/>
+            <input type="text" id="pseudo"  className={styles.input} placeholder="Pseudo" onChange={handleChange}/>
             <input type="password" id="password"  className={styles.input} placeholder="Mot de passe" onChange={handleChange} />
             <button id='submit' className={styles.btnSubmit} onClick={handleChange}>Valider</button>
+            <div id='createAccountContainer__errorMsg' className={styles.createAccountContainer__errorMsg}>Veuillez renseigner correctement les champs.</div>
             <div><Link to="/login">Se connecter</Link></div>
         </div>
     </div>
--- taskflow-kanban/src/components/Login/Login.jsx
+++ taskflow-kanban/src/components/Login/Login.jsx
@@ -3,12 +3,13 @@ import styles from './login.module.css'
 import { Link } from "react-router-dom";
 
 function Login() {
-    const [accountInfos, setAccountInfos] = useState({mail:"", password:""});
+    const [accountInfos, setAccountInfos] = useState({pseudo:'', password:''});
 
     const handleChange = (event) => {
+        //document.getElementById('loginContainer__errorMsg').style.display = "none";
         switch(event.target.id) {
-            case 'email':
-                accountInfos.mail = event.target.value;
+            case 'pseudo':
+                accountInfos.pseudo = event.target.value;
                 setAccountInfos(accountInfos);
                 break;
             case 'password':
@@ -18,7 +19,7 @@ function Login() {
             case 'submit':
                 console.log(accountInfos);
                 console.log('vérification du compte user si valide le rediriger vers le tableau');
-                
+                checkAccountInfos();
                 break;
             default:
                 console.log("problème survenu au niveau des paramètres !");
@@ -26,13 +27,25 @@ function Login() {
         }
         console.log(accountInfos);
     }
+
+    const checkAccountInfos = () => {
+        if (accountInfos.pseudo.length === 0  || accountInfos.password.length === 0) { // non valide
+            // affichage de l'erreur
+            document.getElementById('loginContainer__errorMsg').style.display = "block";
+        }
+        else {
+            document.getElementById('loginContainer__errorMsg').style.display = "none";
+        }
+    }
+    
   return (
     <div className={styles.loginContainer}>
          <div className={styles.container_formAddColumns}>
             <h4>Se connecter</h4>
-            <input type="text" id="email"  className={styles.input} placeholder="Email" onChange={handleChange}/>
+            <input type="text" id="pseudo"  className={styles.input} placeholder="Pseudo" onChange={handleChange}/>
             <input type="password" id="password"  className={styles.input} placeholder="Mot de passe" onChange={handleChange} />
             <button id='submit' className={styles.btnSubmit} onClick={handleChange}>Valider</button>
+            <div id='loginContainer__errorMsg' className={styles.loginContainer__errorMsg}>Veuillez renseigner correctement les champs.</div>
             <div><Link to="/createaccount">Créer un compte</Link></div>
         </div>
     </div>
